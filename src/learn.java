973a699b36535cf26b3d309368481e5fa4d3c0f3
d.molchanov <dnb2011@yandex.ru>
2023-02-12 17:07:27 +0300

    Добавление дополнительных файлов

--- src/learn.java
+++ src/learn.java
@@ -3,5 +3,7 @@ public class learn {
     public static void main(String[] args) {
         System.out.println("Hello test");
         System.out.println("Hello test2");
+        System.out.println("Hello new branch");
+
     }
 }
